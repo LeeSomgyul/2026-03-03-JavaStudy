a8f8f91b9ab8400bb895faab269361ba94898c4b
sist-12 <sist-12@sist12>
2026-03-06 14:57:22 +0900

    20260303

---
+++ Chapter01_자바시작/src/JavaDrive.java
@@ -0,0 +1,9 @@
+
+public class JavaDrive {
+
+	public static void main(String[] args) {
+		System.out.println("자바 실행");
+		System.out.println("<html><body><h1>Hello Java</h1></body></html>");
+	}
+
+}
